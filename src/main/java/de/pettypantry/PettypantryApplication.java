70b135c18c3b2a5cfed3a1b0c211c466eededad9
Arno Heitmann <a.f.heitmann@gmail.com>
2023-10-16 14:27:09 +0200

    chore: add project files and folders

---
+++ src/main/java/de/pettypantry/PettypantryApplication.java
@@ -0,0 +1,13 @@
+package de.pettypantry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettypantryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettypantryApplication.class, args);
+	}
+
+}
